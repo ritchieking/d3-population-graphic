d7fe8ae8d82f09b66ba724c84b230ec94bea75ab
Ritchie King <ritchiesking@gmail.com>
2013-05-26 18:10:45 -0400

    Added a scale and axis. Changed the way width and height are defined to include margins. Added all the text elements to a group so that other text elements (e.g. labels) wouldn't be selected when using selectAll().

--- js/d3code.js
+++ js/d3code.js
@@ -1,21 +1,53 @@
 d3.json("data/worldPop.json",function(data){
 	blob = data;
-	nest = d3.nest().key(function(d){return d.year}).entries(blob)
 
+	// Bar chart worldwide population composition
+
+	// First step is to filter out for only the decadal years. We can do this by using the remainder operator - %. If you divide the year by 10 and the remainder is 0, then you know you have a decade.
+	barBlob = data.filter(function(d){ return d.year%10 == 0})
+
+	// Then, take barBlob and create a nested version of it where the years are used as a hierarchical key. This will allow you to easily 
+	barNest = d3.nest().key(function(d){return d.year}).entries(barBlob)
+
+	// Start with 1950 selected
 	selected = "1950";
 
-	var width = 500,
-		height = 500,
+	var margin = {top: 30, right: 20, bottom: 0, left: 0},
+    	width = 400 - margin.left - margin.right,
+    	height = 250 - margin.top - margin.bottom,
 		font = 13,
 		chartmargin = 100,
-		barHeight = 50;
+		barHeight = (height-4)/4;
 
-	svg = d3.select("body").append("svg")
-		.attr("width", width)
-		.attr("height",height)
+	barScale = d3.scale.linear()
+		.domain([
+			0,
+			d3.max(barBlob, function(d,i){return d.percentage})
+			])
+		.range([
+			chartmargin,
+			width
+			]);
 
-	years = svg.selectAll("text")
-		.data(nest)
+	barAxis = d3.svg.axis()
+		.scale(barScale)
+		.orient("top")
+		.ticks(4)
+		.tickFormat(d3.format("%"))
+
+
+	barSVG = d3.select("#bar-chart").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		
+	barSVG.append("g")
+		.attr("class","x axis")
+		.call(barAxis)
+
+	years = barSVG.append("g").selectAll("text")
+		.data(barNest)
 		.enter()
 		.append("text")
 			.attr("style","cursor:pointer")
@@ -36,12 +68,12 @@ d3.json("data/worldPop.json",function(data){
 			})
 			.on("click",function(d,i){
 				selected = d.key;
-				bars.data(blob.filter(function(d){ return d.year == selected})).enter()
+				bars.data(barBlob.filter(function(d){ return d.year == selected})).enter()
 
 				bars.transition()
 					.duration(500)
 					.attr("width",function(d,i){
-						return d.percentage*600
+						return barScale(d.percentage) - barScale(0)
 					})
 
 				years.transition()
@@ -56,8 +88,8 @@ d3.json("data/worldPop.json",function(data){
 					})
 			})
 
-	bars = svg.selectAll("rect")
-		.data(blob.filter(function(d){ return d.year == selected}))
+	bars = barSVG.selectAll("rect")
+		.data(barBlob.filter(function(d){ return d.year == selected}))
 		.enter()
 		.append("rect")
 		.attr("fill",function(d,i){
@@ -68,13 +100,14 @@ d3.json("data/worldPop.json",function(data){
 				return "darkmagenta"
 			}
 		})
-		.attr("x",chartmargin)
+		.attr("x",barScale(0))
 		.attr("y",function(d,i){
 			return (barHeight+1)*i
 		})
 		.attr("height",barHeight)
 		.attr("width",function(d,i){
-			return d.percentage*600
+			return barScale(d.percentage) - barScale(0)
 		})
+
 })
 
